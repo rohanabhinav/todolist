47773672cd8457ae10a8f4ff14b6f42c1b64b6d1
rohanabhinav <rohanabhinav00@gmail.com>
2024-04-19 14:08:30 +0530

    Changes made

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todolist';
+  constructor(){
+    // setTimeout(() => {
+    //   this.title = "Changed title";
+    // }, 2000);
+  }
 }
